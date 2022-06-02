e60742faf5d88c7e10f3c18242b577411fa5539b
蔡斌伟 <cbw2370657145@163.com>
2022-06-02 14:03:08 +0800

    测试AR形式的增删改查。

--- src/test/java/connect/TestMpAr.java
+++ src/test/java/connect/TestMpAr.java
@@ -1,9 +1,13 @@
 package connect;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.atcaibinwei.mp.bean.Employee;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
 
 public class TestMpAr {
 
@@ -22,5 +26,33 @@ public class TestMpAr {
 
 		boolean re = employee.insert();
 		System.out.printf("re:" + re);
+	}
+
+	@Test
+	public void testARSelect() {
+		Employee employee = new Employee();
+
+		employee = employee.selectById(2);
+		System.out.println(employee);
+
+		List<Employee> employees = employee.selectAll();
+		System.out.println(employees);
+
+		List<Employee> employees1 = employee.selectList(new EntityWrapper<Employee>().like("last_name", "test"));
+		System.out.println(employees1);
+
+		int gender = employee.selectCount(new EntityWrapper().like("gender", "0"));
+		System.out.println(gender);
+
+	}
+
+	@Test
+	public void testARPage() {
+		Employee employee = new Employee();
+		Page<Employee> employeePage = employee.selectPage(new Page<>(1, 2), new EntityWrapper<Employee>());
+		System.out.println(employeePage);
+		System.out.println(employeePage.getRecords());
+
+
 	}
 }
